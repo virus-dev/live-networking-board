ce44399cd63cdfa7e3f775ad346f8050c977881f
PJunhyuk-mac <jgravity@naver.com>
2017-11-23 19:03:33 +0900

    Optimization for invest-networking-board

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('pages/index', { title: '실시간 모의 투자 시스템' });
 });
 
-/* make route to invest page */
-router.get('/invest', function(req, res, next) {
-  res.render('pages/invest', { title: '실시간 모의 투자 시스템' });
-});
-
-/* make route to invest-list page */
-router.get('/invest/list', function(req, res, next) {
-  res.render('pages/invest-list', { title: '실시간 모의 투자 시스템' });
-});
-
-/* make route to result page */
-router.get('/invest/result', function(req, res, next) {
-  res.render('pages/invest-result', { title: '실시간 모의 투자 시스템' });
+/* make route to board page */
+router.get('/board', function(req, res, next) {
+  res.render('pages/board', { title: '실시간 모의 투자 시스템' });
 });
 
 module.exports = router;
