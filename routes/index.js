92c0290b4f3d18730ae7c08f912b7774ee247ca8
PJunhyuk-mac <jgravity@naver.com>
2017-11-25 00:48:53 +0900

    Add board page

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: '실시간 모의 투자 시스템' });
+  res.render('pages/index', { title: '바이러스 네트워킹 프로그램' });
 });
 
-/* make route to board page */
+/* make route to board-admin page */
+router.get('/board', function(req, res, next) {
+  res.render('pages/board', { title: '바이러스 네트워킹 프로그램' });
+});
+
+/* make route to board-admin page */
 router.get('/board-admin', function(req, res, next) {
-  res.render('pages/board-admin', { title: '실시간 모의 투자 시스템' });
+  res.render('pages/board-admin', { title: '바이러스 네트워킹 프로그램' });
 });
 
 module.exports = router;
